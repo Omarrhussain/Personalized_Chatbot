8147eff71633b83b3b78a4d855508e823042fa63
Omarrhussain <93000534+Omarrhussain@users.noreply.github.com>
2025-11-15 18:55:32 +0200

    fix api_server.py

--- api_server.py
+++ api_server.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 if __name__ == "__main__":
     uvicorn.run(
         "src.MLOps.api.app:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
-        reload=True
+        reload=False
     )
